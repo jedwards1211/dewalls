94a4339e6090a4673836d9e54fbdf65399b373ab
Andy Edwards <jedwards@fastmail.com>
2015-12-24 18:10:26 -0600

    comment out tostrings stuff...VC++ is too weak to handle it?

--- test/tostrings.h
+++ test/tostrings.h
@@ -1,14 +1,14 @@
 #ifndef TOSTRINGS_H
 #define TOSTRINGS_H
 
-#include "../lib/catch.hpp"
+//#include "../lib/catch.hpp"
 
-#include <string>
-#include <QString>
+//#include <string>
+//#include <QString>
 
-namespace Catch {
-template<> std::string toString<QString>(QString const& s);
-}
+//namespace Catch {
+//template<> std::string toString<QString>(QString const& s);
+//}
 
 #endif // TOSTRINGS_H
 
